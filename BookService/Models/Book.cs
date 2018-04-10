09a62bce9370d2b21421658292477db96b4f9a2b
Cindy Peng <xipeng@microsoft.com>
2018-04-10 11:20:22 +0800

    initial

---
+++ BookService/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookService.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+        public string Genre { get; set; }
+
+        //Foreign key
+        public int AuthorId { get; set; }
+
+        // Navigation property
+        public Author Author { get; set; }
+
+
+    }
+}
